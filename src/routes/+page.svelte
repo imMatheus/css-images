664159b966b07b2417962bcc86a58c9d73471c80
imMatheus <matheusmendesbarata03@gmail.com>
2023-08-12 23:23:42 +0200

    checkpoint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,10 +20,9 @@
 
 	let imageColors: string[] = [];
 	let img: HTMLImageElement | null = null;
-	let currentPixelIndex = 0;
-	const pixelsPerFrame = 1000; // Adjust this number as needed
 
-	async function processImageChunk() {
+	async function processImageColors() {
+		console.log('n this bitch');
 		if (!img) return;
 
 		const canvas = document.createElement('canvas');
@@ -36,37 +35,24 @@
 			const imageData = context.getImageData(0, 0, img.width, img.height);
 			const data = imageData.data;
 
-			for (
-				let i = currentPixelIndex;
-				i < currentPixelIndex + pixelsPerFrame && i < data.length;
-				i += 4
-			) {
+			console.log(data);
+
+			for (let i = 0; i < data.length; i += 40) {
 				const color = `rgb(${data[i]}, ${data[i + 1]}, ${data[i + 2]})`;
 				imageColors.push(color);
 			}
 
-			currentPixelIndex += pixelsPerFrame;
-
-			imageColors = [...imageColors];
-			if (currentPixelIndex < data.length) {
-				requestAnimationFrame(processImageChunk);
-			}
+			console.log('done: ');
+			console.log(imageColors);
 		}
 	}
 
-	// Call the function when selectedImage changes
 	$: {
-		console.log('asass');
-
-		imageColors.length = 0; // Clear previous image colors
-		currentPixelIndex = 0;
-		if (selectedImage) {
-			console.log('aaaa');
-			processImageChunk();
+		if (img) {
+			imageColors.length = 0; // Clear previous image colors
+			processImageColors();
 		}
 	}
-
-	console.log(imageColors);
 
 	function handleImageLoad(event: Event) {
 		img = event.target as HTMLImageElement;
@@ -94,7 +80,7 @@
 <div class="p-32">
 	<h2 class="text-5xl font-black tracking-wider text-center">Choose your image</h2>
 
-	<div class="h-96 grid grid-cols-2 bg-black">
+	<div class="h-20 w-40 grid grid-cols-2 bg-black">
 		<div class="bg-red-500 relative">
 			{#if selectedImage}
 				<img
